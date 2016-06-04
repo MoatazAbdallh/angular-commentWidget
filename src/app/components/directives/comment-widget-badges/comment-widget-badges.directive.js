12c2bf95f977c7198b96e9fab28e88c57f883489
Moataz Abdallh <moataz.abdallh@gmail.com>
2016-06-05 01:04:41 +0200

    Comment Widget Badges Directive

---
+++ src/app/components/directives/comment-widget-badges/comment-widget-badges.directive.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Moataz on 6/4/2016.
+ */
+
+/**
+ * @ngdoc directive
+ * @name commentWidget.directive:commentWidgetBadges
+ * @scope
+ * @restrict 'AE
+ * @description This is directive responsible for showing comments count and recommendation count badges.
+ */
+
+(function () {
+  'use strict';
+  angular.module("commentWidget")
+    .directive("commentWidgetBadges", commentWidgetBadges);
+
+  /* @ngInject */
+  function commentWidgetBadges(CommentsList) {
+    return {
+      restrict: "EA",
+      templateUrl: "app/components/directives/comment-widget-badges/comment-widget-badges.html",
+      scope: {
+        appName: "="
+      },
+      controller: CommentWidgetBadgesController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    /* @ngInject */
+    function CommentWidgetBadgesController($scope) {
+      var vm = this;
+      vm.appInfo = CommentsList.getAppInfo();
+    }
+  }
+})();
