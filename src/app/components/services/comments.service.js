0ec4e2b6e0a3f31c509ab88fcf591f6053d6db29
Moataz Abdallh <moataz.abdallh@gmail.com>
2016-06-08 02:52:28 +0200

    Updating Comment Service to handle all requests

--- src/app/components/services/comments.service.js
+++ src/app/components/services/comments.service.js
@@ -8,7 +8,7 @@
     .service("CommentsList", CommentsList);
 
   /* @ngInject */
-  function CommentsList($http, $q) {
+  function CommentsList($http, $q,toastr) {
     var CONFIG = null;
     $http.get("config/app-config.json").then(function (result) {
       CONFIG = result.data;
@@ -27,7 +27,7 @@
       }).then(function (result) {
         deferred.resolve(result.data.data);
       }, function (err) {
-        toastr.error("Sorry Couldn't load App Info Please Try Again Later", 'Error');
+        toastr.error("Sorry Couldn't load Comment List, Please Try Again Later", 'Error');
         deferred.reject(err.message);
       });
       return deferred.promise;
@@ -49,6 +49,59 @@
         deferred.reject(err.message);
       });
       return deferred.promise;
-    }
+    };
+    this.incrementRecommendation = function () {
+      var deferred = $q.defer();
+      $http({
+        url: CONFIG.base_url + "increment-recommendation",
+        method: "POST",
+        data: {'app_id': CONFIG.app_id},
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(function (result) {
+        deferred.resolve(result.data.data);
+      }, function (err) {
+        toastr.error("Sorry Couldn't load Comments Recommendation count, Please Try Again Later", 'Error');
+        deferred.reject(err.message);
+      });
+      return deferred.promise;
+    };
+    this.updateCommentItem = function (data) {
+      var deferred = $q.defer();
+      $http({
+        url: CONFIG.base_url + "updateCommentItem",
+        method: "POST",
+        data: data,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(function (result) {
+        deferred.resolve(result.data.data);
+      }, function (err) {
+        toastr.error("Sorry Couldn't update your response, Please Try Again Later", 'Error');
+        deferred.reject(err.message);
+      });
+      return deferred.promise;
+    };
+
+    this.submitComment = function (data) {
+      var deferred = $q.defer();
+      var extendedData = angular.extend(data, {app_id: CONFIG.app_id});
+      $http({
+        url: CONFIG.base_url + "post-comment",
+        method: "POST",
+        data: extendedData,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(function (result) {
+        deferred.resolve(result.data.data);
+      }, function (err) {
+        toastr.error("Sorry Couldn't submit comment in mean time, Please Try Again Later", 'Error');
+        deferred.reject(err.message);
+      });
+      return deferred.promise;
+    };
   }
 })();
