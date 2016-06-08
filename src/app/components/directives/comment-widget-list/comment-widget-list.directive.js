dc4f21e07aa22446e3390c84edf66426967df1ab
Moataz Abdallh <moataz.abdallh@gmail.com>
2016-06-08 02:52:10 +0200

    Updating List directive

--- src/app/components/directives/comment-widget-list/comment-widget-list.directive.js
+++ src/app/components/directives/comment-widget-list/comment-widget-list.directive.js
@@ -7,7 +7,7 @@
     .directive("commentWidgetList", commentWidgetList);
 
   /* @ngInject */
-  function commentWidgetList(CommentsList, moment) {
+  function commentWidgetList(CommentsList, moment, $rootScope) {
     return {
       restrict: "EA",
       templateUrl: "app/components/directives/comment-widget-list/comment-widget-list.html",
@@ -20,12 +20,26 @@
     };
     function CommentWidgetController() {
       var vm = this;
+      vm.updateCommentItem = updateCommentItem;
       CommentsList.getAppComments().then(function (result) {
         vm.commentList = result.map(function (commentItem) {
           commentItem.creation_date = moment(commentItem.creation_date).format("DD-MM-YYYY HH:MM")
           return commentItem
         });
 
+      });
+      function updateCommentItem(type, commentItem) {
+        if (type == "up") {
+          commentItem.points_up++
+        }
+        else
+          commentItem.points_down++;
+
+        CommentsList.updateCommentItem(commentItem);
+      }
+
+      $rootScope.$on("ADDED_NEW_COMMENT", function (event,args) {
+        vm.commentList.push(args);
       });
     }
   }
